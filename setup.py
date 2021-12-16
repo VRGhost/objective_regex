87306c8813279c15c7eca29f1289423dae79d5da
Actions <github+actions@gmail.com>
2021-12-16 13:11:28 +0000

    Bump version: 0.0.6 → 0.0.7

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="objective_regex",
-    version="0.0.6",
+    version="0.0.7",
     author="Ilja Orlovs",
     author_email="vrghost@gmail.com",
     description="Objective regular expressions",
