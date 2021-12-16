15728c9d3a202a159f7bdd172753c6048e140f29
Ilja Orlovs <vrghost@gmail.com>
2021-12-16 12:19:14 +0000

    Bump version: 0.0.1 → 0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="o_re",
-    version="0.0.1",
+    version="0.0.2",
     author="Ilja Orlovs",
     author_email="vrghost@gmail.com",
     description="Objective regular expressions",
